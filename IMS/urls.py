2da64efb838aad289311f945ccca4d4d55a9e7e7
Mrunali <mrunaliparsekar18@gmail.com>
2025-07-26 20:14:25 +0530

    update url.py

--- IMS/urls.py
+++ IMS/urls.py
@@ -19,6 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('auth_system.urls')),
-  
+    path('auth/', include('auth_system.urls')),
 ]
